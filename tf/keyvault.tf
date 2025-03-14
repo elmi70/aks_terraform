d24c64e7faef9fe15b571eccf9c7fffb4684e18e
elmi70 <43352946+elmi70@users.noreply.github.com>
2025-03-14 01:30:35 +0100

    Update keyvault.tf

--- tf/keyvault.tf
+++ tf/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "aks_kv" {
-  name                       = "${var.cluster_name}-kv"
+  name                       = "${var.cluster_name}-kv-somguru"
   location                   = azurerm_resource_group.aks.location
   resource_group_name        = azurerm_resource_group.aks.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
